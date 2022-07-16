466146611318a930a41deb0080cbbda510edcfef
PeterMonev <monevpeter@gmail.com>
2022-07-16 11:55:07 +0300

    Create 03. Even or Odd.js

---
+++ ProgrammingBasic/Conditional Statements-Lab/03. Even or Odd.js
@@ -0,0 +1,15 @@
+function solve(input){
+    let number = Number(input[0])
+
+    if (number % 2 === 0){
+        console.log("even")
+    } else {
+        console.log("odd")
+    }
+        
+    
+}
+solve(["2"])
+solve(["3"])
+solve(["25"])
+solve(["1024"])
