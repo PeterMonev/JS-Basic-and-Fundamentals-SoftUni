63c089042c82c7e2dbb47828a958d550d6bdd2ed
PeterMonev <monevpeter@gmail.com>
2022-07-22 18:45:50 +0300

    Create 04. Personal Titles.js

---
+++ ProgrammingBasic/Conditional Statements Advanced - Lab/04. Personal Titles.js
@@ -0,0 +1,24 @@
+function personalTitles(input){
+    const age = Number(input[0]);
+    const gender = input[1];
+
+    if(gender == "f"){
+        if(age >= 16){
+            console.log("Ms.");
+        } else {
+            console.log("Miss");
+    } 
+
+    } else {
+        if( age >= 16){
+            console.log("Mr.");
+        } else {
+            console.log("Master");
+        }
+    }
+
+
+}
+personalTitles(["12","f"]);
+personalTitles(["17","m"]);
+personalTitles(["25","f"]);
