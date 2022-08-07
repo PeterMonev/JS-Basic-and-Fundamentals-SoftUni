44910cee00a9d08501cfae054cb40f64f216504a
PeterMonev <monevpeter@gmail.com>
2022-08-07 10:54:38 +0300

    Create solve.js

---
+++ ProgrammingBasic/Conditional Statements Advanced - Lab/solve.js
@@ -0,0 +1,19 @@
+function solve(input){
+    let name = "Pesho"
+    switch(name){ 
+        case"Todor": console.log("Hello Todor"); break;
+        case"Petar": console.log("Hello Petar"); break;
+        case"Gosh": console.log("Hello Gosh"); break;
+        default: console.log("Hello ?")
+        
+
+        
+    }
+
+
+
+
+
+
+}
+solve();
