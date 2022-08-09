82dccd4ffb97e9271094dbf94d09c53c19ea3504
PeterMonev <monevpeter@gmail.com>
2022-08-09 12:09:32 +0300

    Solving a task

---
+++ ProgrammingBasic/Conditional Statements Advanced - More Exercises/09.Numbersfrom1to10.js
@@ -0,0 +1,12 @@
+function numberFrom1To10(){
+   let number = 1;
+   let result = 1;
+
+    while(number <= 10){
+       result = number
+         number++
+        console.log(result) 
+    }
+      
+}
+numberFrom1To10()
