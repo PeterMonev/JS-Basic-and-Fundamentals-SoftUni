65cd70f858987cba25b223e730455167c5683657
PeterMonev <monevpeter@gmail.com>
2022-08-10 11:47:02 +0300

    Create inchestoCentimeters.js
    Solving a task.

---
+++ ProgrammingBasic/First Steps in Coding-Lab/inchestoCentimeters.js
@@ -0,0 +1,9 @@
+function inchestoCentimeters(input){
+    const icn = Number (input[0])
+    const a = 2.54
+    const solve  = icn * a
+    
+    console.log(solve)
+    
+}
+inchestoCentimeters(['5'])
