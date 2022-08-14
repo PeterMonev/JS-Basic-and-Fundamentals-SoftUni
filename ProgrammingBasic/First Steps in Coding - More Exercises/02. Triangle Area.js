801d69a81628c8e88ed0aea5bb34d2489eb82c7e
PeterMonev <monevpeter@gmail.com>
2022-08-14 10:25:15 +0300

    Solving a task

---
+++ ProgrammingBasic/First Steps in Coding - More Exercises/02. Triangle Area.js
@@ -0,0 +1,16 @@
+function triangleArea(input){
+    const a = Number (input[0])
+    const h = Number (input[1])
+
+    const area = a * h / 2
+
+    console.log(area.toFixed(2))
+
+
+
+
+
+
+}
+triangleArea([20,30])
+triangleArea([15,35])
