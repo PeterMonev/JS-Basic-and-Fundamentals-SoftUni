f4951d732f9726585be700bf7c1c0a3b2f13976d
PeterMonev <monevpeter@gmail.com>
2022-08-17 09:13:16 +0300

    Solving a task

---
+++ ProgrammingBasic/First Steps in Coding - More Exercises/03. Celsius to Fahrenhein.js
@@ -0,0 +1,13 @@
+function celsiusToFarenheut(input){
+    let celsius = Number(input[0])
+
+    let farenheit = celsius * 1.8 + 32
+    console.log(farenheit.toFixed(2))
+
+
+
+
+
+
+}
+celsiusToFarenheut([25])
