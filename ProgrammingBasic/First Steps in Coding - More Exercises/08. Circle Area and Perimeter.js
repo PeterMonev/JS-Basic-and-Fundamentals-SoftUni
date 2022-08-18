8d9dd8ace5a93779547e2ea36a3a8d3aa4168443
PeterMonev <monevpeter@gmail.com>
2022-08-18 09:23:21 +0300

    Solving a task

---
+++ ProgrammingBasic/First Steps in Coding - More Exercises/08. Circle Area and Perimeter.js
@@ -0,0 +1,23 @@
+function circleareaNPerimeter(input){
+    const r = Number(input[0])
+
+    let area = Math.PI * Math.pow(r,2)
+    let perimeter = 2 * Math.PI * r
+
+    console.log(area.toFixed(2))
+    console.log(perimeter.toFixed(2))
+
+
+
+
+
+
+
+
+
+
+
+
+}
+circleareaNPerimeter([3])
+circleareaNPerimeter([4.5])
