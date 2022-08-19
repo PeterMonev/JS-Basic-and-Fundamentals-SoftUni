af652fb677385895c1db1c5869475581d9b84744
PeterMonev <monevpeter@gmail.com>
2022-08-19 13:44:55 +0300

    Solving a task

---
+++ ProgrammingBasic/For Loop - Lab/07.SumofNumbers.js
@@ -0,0 +1,11 @@
+function sumOFNumbers(input){
+    let n = input[0];
+    let sum = 0;
+
+    for(let i = 0;i < n.length; i++){
+        sum += Number(n[i]);
+        
+    }
+    console.log(`The sum of the digits is:${sum}`) 
+}
+sumOFNumbers(["1234"])
