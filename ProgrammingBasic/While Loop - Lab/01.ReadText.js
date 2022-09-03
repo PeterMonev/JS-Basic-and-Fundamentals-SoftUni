31c5a78e1afed26af6676a5e2ed07b83d5e96acd
PeterMonev <monevpeter@gmail.com>
2022-09-03 12:00:59 +0300

    Solving problem

---
+++ ProgrammingBasic/While Loop - Lab/01.ReadText.js
@@ -0,0 +1,16 @@
+function readText(input){
+    let index = 0;
+    let string = input[index++];
+
+    while(string !== "Stop"){
+        console.log(string);
+        string = input[index++];
+        
+       
+        
+    }
+
+
+
+}
+readText(["Nakov","SoftUni","Sofia","Bulgaria","SomeText","Stop","AfterStop","Europe","HelloWorld"])
