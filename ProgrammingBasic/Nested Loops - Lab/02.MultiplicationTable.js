e012bc1f594d0d8d1a3453f561f3a2e0f2674b70
PeterMonev <monevpeter@gmail.com>
2022-09-09 10:04:49 +0300

    Solving a problem

---
+++ ProgrammingBasic/Nested Loops - Lab/02.MultiplicationTable.js
@@ -0,0 +1,12 @@
+function multiplicationTabel(){
+ 
+    for( let x = 1 ; x <= 10 ; x++){
+      console.log(x);
+         for(let y = 1 ; y <= 10 ; y++){
+       console.log(`${x} * ${y} = ${x*y}`);
+
+        }
+    }
+
+}
+multiplicationTabel()
