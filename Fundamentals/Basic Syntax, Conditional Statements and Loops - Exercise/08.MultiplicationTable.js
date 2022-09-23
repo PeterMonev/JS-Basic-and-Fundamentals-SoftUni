804472d83ce7f3b130aea1835b668b139dede990
PeterMonev <monevpeter@gmail.com>
2022-09-23 10:01:33 +0300

    Solving a task

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/08.MultiplicationTable.js
@@ -0,0 +1,12 @@
+function multiplicationTable(num){
+    for(let i = 1 ; i <= 1 ; i++){
+
+        for(let j = 1 ; j <= 10 ; j++){
+
+            console.log(`${num} X ${j} = ${num*j}`)
+        }
+        
+    }
+
+}
+multiplicationTable(5)
