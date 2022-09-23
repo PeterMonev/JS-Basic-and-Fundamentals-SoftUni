c35f5742c340378a247bfcd378a1d27dc7ec0309
PeterMonev <monevpeter@gmail.com>
2022-09-23 10:01:29 +0300

    Solving a task

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/07.TriangleOfNumbers.js
@@ -0,0 +1,17 @@
+function triangleOfNumbers(num){
+   
+
+    for(let i = 1 ; i <= num ; i++){
+       let prinLine = "";
+        for(let j = 1 ; j <= i ; j++){
+            prinLine += (i + ' ')
+            
+            
+            
+        }
+
+ console.log(`${prinLine}`)
+    }
+   
+}
+triangleOfNumbers(3);
