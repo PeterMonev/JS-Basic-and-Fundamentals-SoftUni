ed823a0a8e05274ff5d42cff1ea03bf80b7b08b3
PeterMonev <monevpeter@gmail.com>
2022-10-02 19:31:12 +0300

    Solving a task

---
+++ Fundamentals/Data Types and Variables - Exercise/08. Calculator.js
@@ -0,0 +1,17 @@
+function calculator(number,operator,number2){
+    switch(operator){
+        case "+":
+            console.log((number + number2).toFixed(2));
+            break;
+        case "-":
+            console.log((number - number2).toFixed(2));
+            break;
+        case "*": 
+            console.log((number * number2).toFixed(2));
+            break;
+        case "/":
+            console.log((number / number2).toFixed(2));
+            break;            
+    }
+}
+calculator(5,'+',10)
