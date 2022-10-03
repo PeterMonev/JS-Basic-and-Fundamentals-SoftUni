2932fcc4420e482a2562b3d9e2aec27a806ee251
PeterMonev <monevpeter@gmail.com>
2022-10-03 19:52:54 +0300

    Solving a task

---
+++ Fundamentals/Data Types and Variables - More Exercise/02.PrimeNumberChecker.js
@@ -0,0 +1,15 @@
+function primeNumberChecker(num){
+   
+    
+    if(num % 2 !=0 && num % 3 !== 0 ){
+      
+        console.log('true')
+        
+    } else {
+        console.log('false')
+    }
+
+}
+primeNumberChecker(7);
+primeNumberChecker(8);
+primeNumberChecker(81);
