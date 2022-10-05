188c0316ab205f34ee0a891262c7382c5eba66ea
PeterMonev <monevpeter@gmail.com>
2022-10-05 09:00:33 +0300

    Solving a task

---
+++ Fundamentals/Arrays - Lab/01. Sum FirstandLastArrayElements.js
@@ -0,0 +1,11 @@
+function sumFirstandLastStand(numbers){
+    let firstnum = numbers[0];
+    let lastnum = numbers[numbers.length - 1];
+    console.log(firstnum + lastnum);
+
+
+
+}
+sumFirstandLastStand([20, 30, 40]);
+sumFirstandLastStand([10, 17, 22, 33]);
+sumFirstandLastStand([11, 58, 69]);
