a9fbeb499a732586402254adf77196832772f16e
PeterMonev <monevpeter@gmail.com>
2022-10-21 08:54:56 +0300

    Solving a task

---
+++ Fundamentals/Functions - Lab/02.MathPower.js
@@ -0,0 +1,12 @@
+function mathPower(x,n){
+    let result = 0; 
+
+     // for(let i=0;i<n;i++){
+    //      result *= x
+   // }
+     result = Math.pow(x,n)
+    console.log(result);
+}
+mathPower(2,8);
+mathPower(3,4);
+
