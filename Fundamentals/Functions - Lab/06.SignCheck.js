d925f739e333e2ac0a8bb11485c314d7d6f209df
PeterMonev <monevpeter@gmail.com>
2022-10-22 09:38:40 +0300

    Solving a task

---
+++ Fundamentals/Functions - Lab/06.SignCheck.js
@@ -0,0 +1,15 @@
+function signCheck(n1, n2, n3) {
+
+    if(n1 < 0 && n2 > 0 && n3 > 0 || n1 > 0 && n2 < 0 && n3 > 0 || n1 > 0 && n2 > 0 && n3 < 0){
+        console.log("Negative");
+        
+    } else if (n1 < 0 && n2 < 0 && n3 > 0 || n1 < 0 && n2 > 0 && n3 < 0  || n1 > 0 && n2 < 0 && n3 < 0  ){
+        console.log("Positive");
+
+    } else if (n1 < 0 && n2 < 0 && n3 < 0){
+        console.log("Negative");
+    } else {
+        console.log("Positive")
+    }
+}
+signCheck(5, 12, -15);
