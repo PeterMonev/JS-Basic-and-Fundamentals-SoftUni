148949f6f437241167821cd2d970441d35781ea4
PeterMonev <monevpeter@gmail.com>
2022-11-05 06:07:42 +0200

    Solving a task

---
+++ Fundamentals/Regular Expressions - Lab/01.MatchFullName.js
@@ -0,0 +1,15 @@
+function matchFullName(input){
+   let validNames = [];
+   let pattern = /(\b[A-Z][a-z]+ [A-Z][a-z]+\b)/g;
+   
+   let validName;
+
+   while(validName = pattern.exec(input)){
+      validNames.push(validName[0]);
+   }
+
+   console.log(validNames.join(' '));
+
+
+}
+matchFullName("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov")
