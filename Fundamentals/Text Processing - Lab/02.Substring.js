0d32f6c27b55207dbe08d0d6d8101d6a9d941e40
PeterMonev <monevpeter@gmail.com>
2022-11-10 09:11:03 +0200

    Solving a task

---
+++ Fundamentals/Text Processing - Lab/02.Substring.js
@@ -0,0 +1,7 @@
+function substring(string,startIndex,count) {
+    
+    console.log(string.substring(startIndex,count+startIndex).trim())
+
+}
+substring('ASentence', 1, 8)
+substring('SkipWord', 4, 7)
