3fee962400293949facc7264a74cc8b3c7da511a
PeterMonev <monevpeter@gmail.com>
2022-11-12 10:36:59 +0200

    Solving a task

---
+++ Fundamentals/Text Processing - More Exercise/03.DeserializeString.js
@@ -0,0 +1,17 @@
+function deserializeString(input) {
+     
+    let commnad = input.shift();
+
+    while(commnad[0] != 'end'){
+       let char = commnad.split('/')
+
+        commnad.shift();
+   }
+    
+
+
+}
+deserializeString(['a:0/2/4/6',
+'b:1/3/5',
+'end']
+);
